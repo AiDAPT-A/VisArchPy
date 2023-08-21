7c5d30deb2a8b68cff52dae307d891c918c8260e
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-08-21 16:52:55 +0200

    change name of test module for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+"""
+Unit test for the pypdf2.py script
+"""
+from aidapta import utils
+import pathlib
+
+def test_create_output_dir():
+
+    # Fixtures 
+    # base_path= './tests'
+    # dir_name= 'temp'
+    # full_path = pathlib.Path.joinpath(base_path, dir_name)
+
+    # # test if directory was created in the input path
+    # utils.create_output_dir(base_path, dir_name)
+
+    # assert full_path.exists() == True
+
+    pass
