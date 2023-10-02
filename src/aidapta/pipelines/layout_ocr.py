e2620d31a5d6c11e59c5c073b218a2bc38a14d29
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-10-02 12:03:33 +0200

    enable cli

--- src/aidapta/pipelines/layout_ocr.py
+++ src/aidapta/pipelines/layout_ocr.py
@@ -475,11 +475,11 @@ def pipeline(entry_id:str, data_directory: str, output_directory: str, temp_dire
   
 if __name__ == "__main__":
     
-    # app()
+    app()
 
-    pipeline("01960",
-            "/home/manuel/Documents/devel/desing-handbook/data-pipelines/data/pdf-issues/",
-            "/home/manuel/Documents/devel/desing-handbook/data-pipelines/data/test/",
-            "/home/manuel/Documents/devel/desing-handbook/data-pipelines/data/test/tmp/"
-            )
+    # pipeline("01960",
+    #         "/home/manuel/Documents/devel/desing-handbook/data-pipelines/data/pdf-issues/",
+    #         "/home/manuel/Documents/devel/desing-handbook/data-pipelines/data/test/",
+    #         "/home/manuel/Documents/devel/desing-handbook/data-pipelines/data/test/tmp/"
+    #         )
     
