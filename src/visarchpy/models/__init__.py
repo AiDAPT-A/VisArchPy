9ee61e8b748a4c6deab7d6b4167ccfb3ea22a0f7
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-14 22:09:19 +0100

    Add KmeansBbox20 model to VisArchPy package

---
+++ src/visarchpy/models/__init__.py
@@ -0,0 +1,21 @@
+"""
+Loads pretrained models for use in the VisArchPy package.
+"""
+
+import pickle
+
+class KmeansBbox20:
+    """
+    Kmeans model for Bbox plot with 20 clusters.
+    """
+
+    def __init__(self):
+
+        with open('./src/visarchpy/models/kmeans_bbox20.pkl', 'rb') as f: 
+                predictor = pickle.load(f)
+
+        self.predictor = predictor
+
+
+    def __call__(self):
+        return self.predictor
