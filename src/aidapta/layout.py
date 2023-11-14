67ff219645b81a67ad79b11dd168cd0047ecb1e3
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-14 13:50:40 +0100

    Comment out PyPDF2 import and remove unused function

--- src/aidapta/layout.py
+++ src/aidapta/layout.py
@@ -5,7 +5,7 @@ Author: M.G. Garcia
 """
 import os
 import pathlib
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
 from pdfminer.high_level import extract_pages
 from aidapta.utils import create_output_dir
 from pdfminer.layout import (
@@ -19,52 +19,51 @@ from pdfminer.layout import (
     LTCurve
 )
 
-# From https://pypdf2.readthedocs.io/en/latest/user/extract-images.html
 
-
-def extract_images(pdf_file: str, output_dir: str) -> None:
-    """
-    extracts image from a PDF file
+# TODO: test if this code can be removed. It belongs to the old version of the code
+# def extract_images(pdf_file: str, output_dir: str) -> None:
+#     """
+#     extracts image from a PDF file
     
-    Parameters
-    ----------
-    pdf_file: str
-        path to the PDF file
-    output_dir: str 
-        path to directory to extract images. Outputs
-        are organized in folder based on the name of the input PDF
+#     Parameters
+#     ----------
+#     pdf_file: str
+#         path to the PDF file
+#     output_dir: str 
+#         path to directory to extract images. Outputs
+#         are organized in folder based on the name of the input PDF
     
-    Returns
-    -------
-    None
-    """
+#     Returns
+#     -------
+#     None
+#     """
     
-    # open PDF document
-    reader = PdfReader(pdf_file)
+#     # open PDF document
+#     reader = PdfReader(pdf_file)
 
-    # prepare output directory
-    pdf_file_name = pathlib.Path(pdf_file).stem
-    output_directory = create_output_dir(output_dir, pdf_file_name)
+#     # prepare output directory
+#     pdf_file_name = pathlib.Path(pdf_file).stem
+#     output_directory = create_output_dir(output_dir, pdf_file_name)
 
-    for page_index in range(0,len(reader.pages)):
-        page = reader.pages[page_index]
+#     for page_index in range(0,len(reader.pages)):
+#         page = reader.pages[page_index]
 
-        # TODO: fix issue with ValueError: not enough data in PIL
-        try:
-            count=1
-            print('page/img index', page_index, count)
+#         # TODO: fix issue with ValueError: not enough data in PIL
+#         try:
+#             count=1
+#             print('page/img index', page_index, count)
        
-            for image_file_object in page.images:
-                print(image_file_object)
+#             for image_file_object in page.images:
+#                 print(image_file_object)
                 
-                with open(str(output_directory)+'/' + 'page' +str(page_index) +'-'+str(count) + 
-                          image_file_object.name, "wb") as fp:
-                    fp.write(image_file_object.data)
-                    count += 1
-        except ValueError:
-            print("error")
+#                 with open(str(output_directory)+'/' + 'page' +str(page_index) +'-'+str(count) + 
+#                           image_file_object.name, "wb") as fp:
+#                     fp.write(image_file_object.data)
+#                     count += 1
+#         except ValueError:
+#             print("error")
 
-    return None
+#     return None
 
 
 def sort_layout_elements(page:LTPage, img_width:int = None, img_height:int = None)-> dict:
