42c067d4ee1afc272948cc5b2c63ab87c55194ec
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-16 15:50:38 +0100

    apply flake8 rules

--- src/visarchpy/metadata.py
+++ src/visarchpy/metadata.py
@@ -1,5 +1,5 @@
 """
-Dataclasses for handling metadata for extracted visuals
+Dataclasses for handling metadata
 Author: M.G. Garcia
 """
 
@@ -27,12 +27,12 @@ class FilePath:
 
     def update_root_path(self, root_path: str) -> None:
         """Updates the root path of the file path
-        
+
         Parameters
         ----------
         root_path: str
             new root path
-        
+
         Returns
         -------
         None
@@ -41,21 +41,22 @@ class FilePath:
 
     def full_path(self) -> str:
         """Returns the full path of the file path
-        
+
         Returns
         -------
         str
             full path of the file path
         """
         return str(os.path.join(self.root_path, self.file_path))
-    
+
     def __str__(self) -> str:
         return os.path.join(self.root_path, self.file_path)
+
 
 @dataclass
 class Person:
     """
-    Represents a person 
+    Represents a person
     """
     name: str
     role: str
@@ -83,8 +84,9 @@ class Document:
     """
     Represents a document
     """
-    location: FilePath = field(init=True, default=None) # location where the visual is stored
-    
+    # location where the visual is stored
+    location: FilePath = field(init=True, default=None)
+
     def update_root_path(self, path: str) -> None:
         """Updates the root path of the file path """
         self.location.update_root_path(path)
@@ -95,27 +97,29 @@ class Visual:
     """A class for handling metadata for architectural visuals
     extracted from PDF files"""
 
-   
-    document: Document # document where the visual is located
-    document_page: int # page number in the document index
-    bbox: List[int] # bounding box of the visual in the document page
-    bbox_units: str # units of the bounding box
-    id : Optional[str] = field(init=False) # unique identifier
-    caption: Optional[list] = field(init=False, default=None) # caption of the visual    
-    visual_type: Optional[str] = field(init=False, default=None) # one of: photo, drawing, map, etc
-    location: FilePath = field(init=False, default=None) # location where the visual is stored
+    document: Document  # document where the visual is located
+    document_page: int  # page number in the document index
+    bbox: List[int]  # bounding box of the visual in the document page
+    bbox_units: str  # units of the bounding box
+    id: Optional[str] = field(init=False)  # unique identifier
+    # caption of the visual
+    caption: Optional[list] = field(init=False, default=None)
+    # one of: photo, drawing, map, etc.
+    visual_type: Optional[str] = field(init=False, default=None)
+    # location where the visual is stored
+    location: FilePath = field(init=False, default=None)
 
     def __post_init__(self):
         self.id = str(uuid.uuid4())
 
     def set_visual_type(self, visual_type: str) -> None:
         """Sets the visual type. One of photo, drawing, map, etc.
-        
+
         Parameters
         ----------
         visual_type: str
             type of visual
-        
+
         Returns
         -------
         None
@@ -124,12 +128,12 @@ class Visual:
 
     def set_caption(self, caption: str) -> None:
         """Sets the caption for the visual
-        
+
         Parameters
         ----------
         caption: str
             caption for the visual
-        
+
         Returns
         -------
         None
@@ -141,23 +145,25 @@ class Visual:
 
         """
 
-        if self.caption is None: 
-            self.caption = [ caption ]
+        if self.caption is None:
+            self.caption = [caption]
         elif len(self.caption) < 2:
             self.caption.append(caption)
         else:
-            raise Warning(f"Maximum number of captions already set. Ignoring caption: {self.caption}")
-        
+            raise Warning(f"Maximum number of captions already set. Ignoring \
+                          caption: {self.caption}")
+
     def set_location(self, location: FilePath, update: bool = False) -> None:
         """Sets the location where the visual is stored
-        
+
         Parameters
         ----------
         location: FilePath
             location where the visual is stored
         update: bool
-            if True, the root_path of location will be updated. If False, an error will be raised
-        
+            if True, the root_path of location will be updated. If False,
+            an error will be raised if the location is already set
+
         Returns
         -------
         None
@@ -179,67 +185,70 @@ class Visual:
 @dataclass
 class Metadata:
     """
-    Represents metadata of an entry in the library repository
+    Represents the collection of metadata of an entry.
+    An entry consits of a MODS file and zero or mor PDF files.
+    Most of the fields are based on the MODS standard.
     """
 
     documents: List[Document] = field(init=False, default=None)
 
     persons: List[Person] = field(init=False, default=None)
     faculty: Faculty = field(init=False, default=None)
-    mods_file: str = field(init=False) # location of the MODS file
+    mods_file: str = field(init=False)  # location of the MODS file
 
     title: str = field(init=False)
     abstract: str = field(init=False)
-    submission_date: str = field(init=False) # year, month, day
-    thesis_type: str = field(init=False) # master or bachelor thesis
-    subjects: List = field(init=False) # list of subjects defined in repository
-    copyright: str = field(init=False) 
-    languages: List[dict] = field(init=False) # list of languages
+    submission_date: str = field(init=False)  # year, month, day
+    thesis_type: str = field(init=False)  # master or bachelor thesis
+    # list of subjects defined in repository
+    subjects: List = field(init=False)
+    copyright: str = field(init=False)
+    languages: List[dict] = field(init=False)  # list of languages
     uuid: Optional[str] = field(init=False, default=None)  # unique identifier
     # identifiers: List = field(init=False) #
-    iid: str = field(init=False) # internal identifier
-    media_type: List = field(init=False) # internet media type
-    issuance: List = field(init=False) # type of issuance
-    digital_origin: str = field(init=False) # digital origin
-    doi: str = field(init=False) # digital object identifier
-    edition: str = field(init=False) # edition
-    extent: List = field(init=False) # extent
-    form: List = field(init=False) # form
-    classification: List = field(init=False) # classification
-    collection: str = field(init=False) # collection
-    geo_code: List = field(init=False) # geographic code
-    corp_names: List = field(init=False) # corporate names
-    creators: List = field(init=False) # creators
-    physical_description: List = field(init=False) # physical description
-    physical_location: List = field(init=False) # physical location
-    pid: str = field(init=False) # persistent identifier
-    publication_place: List = field(init=False) # publication place
-    publisher: List = field(init=False) # publisher
-    purl: List = field(init=False) # persistent URL
-    type_resource: str = field(init=False) # type of resource
-    web_url: str = field(init=False, default=None) # URL at Educational Repository
+    iid: str = field(init=False)  # internal identifier
+    media_type: List = field(init=False)  # internet media type
+    issuance: List = field(init=False)  # type of issuance
+    digital_origin: str = field(init=False)  # digital origin
+    doi: str = field(init=False)  # digital object identifier
+    edition: str = field(init=False)  # edition
+    extent: List = field(init=False)  # extent
+    form: List = field(init=False)  # form
+    classification: List = field(init=False)  # classification
+    collection: str = field(init=False)  # collection
+    geo_code: List = field(init=False)  # geographic code
+    corp_names: List = field(init=False)  # corporate names
+    creators: List = field(init=False)  # creators
+    physical_description: List = field(init=False)  # physical description
+    physical_location: List = field(init=False)  # physical location
+    pid: str = field(init=False)  # persistent identifier
+    publication_place: List = field(init=False)  # publication place
+    publisher: List = field(init=False)  # publisher
+    purl: List = field(init=False)  # persistent URL
+    type_resource: str = field(init=False)  # type of resource
+    # URL at Educational Repository
+    web_url: str = field(init=False, default=None)
     # total number of images/visuals extracted from the PDF files for
     # this entry in the repository
     total_visuals: Optional[int] = field(init=False, default=0)
     visuals: Optional[List[Visual]] = field(init=False, default=None)
 
-
-    # pdf_location: Optional[str] = field(init=False, default=None) # location of the PDF file
+    #  pdf_location: Optional[str] = field(init=False, default=None) # location of the PDF file
 
     def set_metadata(self, metadata: dict) -> None:
-        """ Sets metadata for a repository entry 
-        
+        """ Sets metadata for a repository entry
+
         Parameters
         ----------
         metadata: dict
             dictionary with metadata from MODS file
-        
+
         Returns
         -------
         None
-        
+
         """
-        
+
         self.persons = metadata.get('persons')
         self.faculty = metadata.get('faculty')
         self.mods_file = metadata.get('modsfile')
@@ -247,9 +256,9 @@ class Metadata:
         self.title = metadata.get('title')
         self.abstract = metadata.get('abstract')
         self.submission_date = metadata.get('date')
-        self.thesis_type = metadata.get('genre') 
+        self.thesis_type = metadata.get('genre')
         self.subjects = metadata.get('subjects')
-        self.copyright = metadata.get('rights') 
+        self.copyright = metadata.get('rights')
         self.languages = metadata.get('language')
 
         self.uuid = metadata.get('identifiers')
@@ -275,13 +284,13 @@ class Metadata:
         self.type_resource = metadata.get('type_resource')
 
     def add_document(self, document: Document) -> None:
-        """ Adds a document object to the metadata 
-        
+        """ Adds a document object to the metadata
+
         Parameters
         ----------
         document: Document
             document object
-        
+
         Returns
         -------
         None
@@ -290,21 +299,21 @@ class Metadata:
         if not self.documents:
             self.documents = []
         self.documents.append(document)
-    
+
     def add_pdf_location(self, path_pdf: str, overwrite: bool = False) -> None:
         """ Sets location of the PDF file 
-        
+
         Parameters
         ----------
         path_pdf: str
             path to the PDF file
         overwrite: bool
             if True, overwrites the PDF location if it is already set
-        
+
         Returns
         -------
         None
-        
+
         Raises
         ------
         ValueError
@@ -312,20 +321,21 @@ class Metadata:
         """
 
         if self.pdf_location and overwrite:
-            raise ValueError('PDF location already set. User overwrite=True to overwrite it.')
+            raise ValueError('PDF location already set. User overwrite=True to\
+                             overwrite it.')
         else:
             self.pdf_location = path_pdf
 
     def add_web_url(self, base_url: str, overwrite: bool = False) -> None:
-        """ Adds a URL to the metadata 
-        
+        """ Adds a URL to the metadata
+
         Parameters
-        ----------  
+        ----------
         base_url: str
             base URL of the repository
         overwrite: bool
-            if True, overwrites the web URL if it is already set           
-        
+            if True, overwrites the web URL if it is already set
+
         Returns
         -------
         None
@@ -334,30 +344,31 @@ class Metadata:
         ------
         ValueError
             if web URL is already set and overwrite is False
-        
+
         """
 
-        if self.web_url and overwrite == False:
-            raise ValueError('base URL already set. User overwrite=True to overwrite it.')
+        if self.web_url and overwrite is False:
+            raise ValueError('base URL already set. User overwrite=True to\
+                             overwrite it.')
         else:
             if self.uuid is not None:
-                if self.uuid[:5] == 'uuid:': # some uuids start with uuid:
-                    self.web_url = f'{base_url}{self.uuid}' 
-                else: # pure uuids do not start with uuid:
+                if self.uuid[:5] == 'uuid:':  # some uuids start with uuid:
+                    self.web_url = f'{base_url}{self.uuid}'
+                else:  # pure uuids do not start with uuid:
                     self.web_url = f'{base_url}uuid:{self.uuid}'
-    
+
     def add_visual(self, visual: Visual) -> None:
         """ Adds a visual to the metadata 
-        
+
         Parameters
         ----------
         visual: Visual
             visual object
-        
+
         Returns
         -------
         None
-        
+
         """
         if not self.visuals:
             self.visuals = []
@@ -369,14 +380,14 @@ class Metadata:
     def as_dict(self) -> dict:
         """ Returns metadata as a dictionary """
         return asdict(self)
-    
+
     def as_dataframe(self) -> pd.DataFrame:
         """ Returns metadata as a Pandas DataFrame """
         return pd.DataFrame([self.as_dict()])
-    
+
     def save_to_csv(self, filename: str) -> None:
-        """ Writes metadata to a CSV file 
-        
+        """ Writes metadata to a CSV file
+
         Parameters
         ----------
         filename: str
@@ -385,19 +396,19 @@ class Metadata:
         Returns
         -------
         None
-        
+
         """
 
         self.as_dataframe().to_csv(filename, index=False)
-    
+
     def save_to_json(self, filename: str) -> None:
         """ Writes metadata to a JSON file 
-        
+
         Parameters
         ----------
         filename: str
             name of the JSON file
-        
+
         Returns
         -------
         None
@@ -405,7 +416,7 @@ class Metadata:
 
         with open(filename, 'w') as f:
             json.dump(self.as_dict(), f, indent=4)
-    
+
 
 def main() -> None:
     from visarchpy.utils import extract_mods_metadata
