8c8a01ea1ddac1764a937a0b8ecc283ff275314f
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-16 18:40:18 +0100

    Refactor CLI OCR code to improve readability and add TODOs for future implementation

--- src/visarchpy/cli/ocr.py
+++ src/visarchpy/cli/ocr.py
@@ -15,16 +15,17 @@ def from_file() -> None:
 
     # TODO: implement extraction of visuals from a single PDF file.
     pass
-    
+
 
 @app.command(help="Extract visuals from all PDF files in a directory.")
 def from_dir(
-    directory: str = typer.Argument(help="Path to directory containing PDF files"),
-    output: str = typer.Argument(help="Path to output directory"),
-    ) -> None:
+    directory: str = typer.Argument(help="Path to directory containing PDF\
+    files"),
+        output: str = typer.Argument(help="Path to output directory")) -> None:
+
     # TODO: implement extraction of visuals from all PDF files in a directory.
     return None
 
+
 if __name__ == "__main__":
     app()
-    
