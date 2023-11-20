a8fb8f3ab094969b91524567ed932ea206099aea
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-20 17:22:05 +0100

    implement batch command

--- src/visarchpy/cli/layout_ocr.py
+++ src/visarchpy/cli/layout_ocr.py
@@ -3,13 +3,18 @@
 import os
 import typer
 from typing_extensions import Annotated, Optional
-from visarchpy.pipelines.layout_ocr import pipeline
+from visarchpy.pipelines import LayoutOCR
+import json
 
-app = typer.Typer(help="Extract visuals from PDF files using layout and\
-                  OCR analysis.",
+app = typer.Typer(help="Extract visuals from PDF files using layout and \
+OCR analysis.",
                   context_settings={"help_option_names": ["-h", "--help"]},
                   add_completion=False)
 
+with open("../default-settings.json", "r") as f:
+    default_settings = json.load(f)
+
+    # src/visarchpy/default-settings.json
 
 @app.command(help="Extract visuals from a single PDF file.")
 def from_file() -> None:
@@ -27,6 +32,12 @@ def from_dir(
     return None
 
 
+
+@app.command(help="Show default settings for the pipeline.")
+def settings() -> None:
+    """Show default settings for the pipeline."""
+    typer.echo(default_settings)
+
 @app.command(help="batch processing for TU Delft's dataset")
 def batch(entry_range: str = typer.Argument(help="Range of entries to process\
                                             e.g.: 1-10"),
@@ -34,27 +45,37 @@ def batch(entry_range: str = typer.Argument(help="Range of entries to process\
                                                containing MODS and pdf files"),
           output_directory: str = typer.Argument(help="path to directory where\
                                                  results will be saved"),
+          settings_file: Annotated[Optional[str],
+                                   typer.Argument(help="path to pipeline JSON settings\
+                                             file. If None default settings will be\
+                                             used. Use: [COMMAND] setting, to see\
+                                             current settings")]= None,
           temp_directory: Annotated[Optional[str],
                                     typer.Argument(help="temporary directory")
-                                    ] = None
+                                    ] = None,
           ) -> None:
     """Extracts metadata from MODS files and images from PDF files
-      using layout and OCR analysis."""
+      using layout and OCR pipeline"""
+
+    if settings_file is None:
+        settings = default_settings
+    else:
+        with open(settings_file, "r") as f:
+            settings = json.load(f)
 
     start_id = int(entry_range.split("-")[0])
     end_id = int(entry_range.split("-")[1])
-    DATA_DIR = data_directory
 
     for id in range(start_id, end_id+1):
         str_id = str(id).zfill(5)
 
-        MODS_FILE = os.path.join(DATA_DIR, str_id + "_mods.xml")
+        MODS_FILE = os.path.join(data_directory, str_id + "_mods.xml")
 
-        pipeline(data_directory,
-                 output_directory,
-                 metadata_file=MODS_FILE,
-                 temp_directory=temp_directory)
+        LayoutOCR(data_directory, output_directory,
+                  settings=settings, metadata_file=MODS_FILE,
+                  temp_directory=temp_directory)
 
+        # TODO: test batch cli
 
 if __name__ == "__main__":
     app()
