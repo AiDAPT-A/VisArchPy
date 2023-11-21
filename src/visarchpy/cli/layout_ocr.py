d2c3d13d128574fa1398b1b52aa0e224dc1203ae
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-21 09:25:12 +0100

    Update layout_ocr.py with new CLI commands and options

--- src/visarchpy/cli/layout_ocr.py
+++ src/visarchpy/cli/layout_ocr.py
@@ -2,9 +2,11 @@
 
 import os
 import typer
-from typing_extensions import Annotated, Optional
+from typing_extensions import Annotated
 from visarchpy.pipelines import LayoutOCR
 import json
+from visarchpy.utils import create_output_dir
+import shutil
 
 app = typer.Typer(help="Extract visuals from PDF files using layout and \
 OCR analysis.",
@@ -14,25 +16,49 @@ OCR analysis.",
 with open("../default-settings.json", "r") as f:
     default_settings = json.load(f)
 
-    # src/visarchpy/default-settings.json
 
 @app.command(help="Extract visuals from a single PDF file.")
-def from_file() -> None:
+def from_file(
+    pdf_file: str = typer.Argument(help="Path to directory containing PDF files."),
+    output_directory: str = typer.Argument(help="Path to directory where results will be saved."),
+    settings: Annotated[str, typer.Option(help="Path to pipeline JSON setting file. If None default settings will be used. Use: [COMMAND] settings, to see current settings.")] = None,
+    mods: Annotated[str, typer.Option(help="Path to MODS file. If None, metadata extraction will be skiped.")] = None
+    ) -> None:
 
-    # TODO: implement extraction of visuals from a single PDF file.
-    pass
+    if settings is None:
+        settings = default_settings
+    else:
+        with open(settings, "r") as f:
+            settings = json.load(f)
+            print('loaded settings from file: ', settings)
+
+    # Create a temporary directory where the input PDF
+    # is copied to be able to use the pipeline class
+    # This could be improve.
+    file_dir = os.path.dirname(pdf_file)
+    temp_directory = create_output_dir(file_dir, './.visarchpy')
+    data_directory = str(temp_directory) + '/'  # TODO: avoid this patching. Manage paths with os.path
+    shutil.copy2(pdf_file, data_directory)
+
+    pipeline = LayoutOCR(data_directory, output_directory,
+                             settings=settings, metadata_file=mods,
+                             ignore_id=True)
+
+    pipeline.run()
+
+    # clean up
+    if temp_directory.exists():
+        shutil.rmtree(temp_directory)
 
 
 @app.command(help="Extract visuals from all PDF files in a directory.")
 def from_dir(
     data_directory: str = typer.Argument(help="Path to directory containing PDF files."),
     output_directory: str = typer.Argument(help="Path to directory where results will be saved."),
-    settings: Annotated[str, typer.Option(help="Path to pipeline JSON setting file. If None default settings will be used. Use: visarch settings, to see current settings.")] = None,
-    mods_file: Annotated[str, typer.Option(help="Path to MODS file. If None, metadata extraction will be skiped.")] = None,
-    tmp_dir: Annotated[str,
-                       typer.Option(help="If provided, PDF files in the data directory will be copied to this directory.")
-                       ] = None
-    ) -> None:
+    settings: Annotated[str, typer.Option(help="Path to pipeline JSON setting file. If None default settings will be used. Use: [COMMAND] settings, to see current settings.")] = None,
+    mods: Annotated[str, typer.Option(help="Path to MODS file. If None, metadata extraction will be skiped.")] = None,
+    tmp: Annotated[str, typer.Option(help="If provided, PDF files in the data directory will be copied to this directory.")
+    ] = None) -> None:
     
     if settings is None:
         settings = default_settings
@@ -42,8 +68,8 @@ def from_dir(
             print('loaded settings from file: ', settings)
 
     pipeline = LayoutOCR(data_directory, output_directory,
-                             settings=settings, metadata_file=mods_file,
-                             temp_directory=tmp_dir, ignore_id=True)
+                             settings=settings, metadata_file=mods,
+                             temp_directory=tmp, ignore_id=True)
 
     pipeline.run()
 
@@ -58,11 +84,9 @@ def settings() -> None:
 def batch(entry_range: str = typer.Argument(help="Range of entries to process, e.g.: 1-10."),
           data_directory: str = typer.Argument(help="path to directory containing MODS and PDF files."),
           output_directory: str = typer.Argument(help="path to directory where results will be saved."),
-          settings: Annotated[str, typer.Option(help="path to pipeline JSON setting file. If None default settings will be used. Use: visarch settings, to see current settings.")] = None,
-          tmp_dir: Annotated[str,
-                                    typer.Option(help="If provided, PDF files in the data directory will be copied to this directory.")
-                                    ] = None
-          ) -> None:
+          settings: Annotated[str, typer.Option(help="path to pipeline JSON setting file. If None default settings will be used. Use: [COMMAND] settings, to see current settings.")] = None,
+          tmp: Annotated[str, typer.Option(help="If provided, PDF files in the data directory will be copied to this directory.")
+            ] = None) -> None:
     """Extracts metadata from MODS files and images from PDF files
       using layout and OCR pipeline"""
 
@@ -82,7 +106,7 @@ def batch(entry_range: str = typer.Argument(help="Range of entries to process, e
 
         pipeline = LayoutOCR(data_directory, output_directory,
                              settings=settings, metadata_file=MODS_FILE,
-                             temp_directory=tmp_dir)
+                             temp_directory=tmp)
 
         pipeline.run()
 
@@ -90,4 +114,11 @@ def batch(entry_range: str = typer.Argument(help="Range of entries to process, e
 if __name__ == "__main__":
     app()
 
-    # batch 2-2 ../../../tests/data/00001/ ../../../tests/data/layout/  ../../../tests/data/tmp/ 
+    # CLI examples:
+    
+    # batch 1-2 ../../../tests/data/00001/ ../../../tests/data/layout/  ../../../tests/data/tmp/ 
+
+    # from-dir ../../../tests/data/00001/ ../../../tests/data/layout/  --tmp-dir ../../../tests/data/tmp/
+
+    # from-file ../../../tests/data/00001/00002_sample.pdf ../../../tests/data/layout/ --mods ../../../tests/data/00001/00002_mods.xml
+
