8eb8ee3a4294b228ca74336fdec1f2c8741ee502
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-27 12:38:40 +0100

    Add viz module to CLI

--- src/visarchpy/cli/main.py
+++ src/visarchpy/cli/main.py
@@ -7,10 +7,10 @@ import visarchpy.cli.layout_ocr as layout_ocr
 import visarchpy.cli.layout as layout
 import visarchpy.cli.ocr as ocr
 import visarchpy.cli.dino as dino
+import visarchpy.cli.viz as viz
 
 app = typer.Typer(
-    help="VisArchPy: data pipelines for extraction and visual analysis of "
-         "architectural visuals in Python.",
+    help="VisArchPy: Data pipelines for extraction, transformation and visualization of architectural visuals in Python.",
     context_settings={
         "help_option_names": ["-h", "--help"]
         },
@@ -21,6 +21,7 @@ app.add_typer(layout.app, name="layout")
 app.add_typer(ocr.app, name="ocr")
 app.add_typer(layout_ocr.app, name="layoutocr")
 app.add_typer(dino.app, name="dino")
+app.add_typer(viz.app, name="viz")
 
 
 if __name__ == "__main__":
